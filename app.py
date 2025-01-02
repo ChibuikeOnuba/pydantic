2a3876852872b9bdc36578094a5874aec8dfe2f6
ChibuikeOnuba <onubawinner042@gmail.com>
2025-01-02 01:39:55 -0800

    json commit

--- app.py
+++ app.py
@@ -19,10 +19,10 @@ class Employee(BaseModel):
     employee_id: UUID = uuid4
     elected_benefits: bool
 
-    @field_validator("employee_id")
+    @field_validator("salary")
     def validate_account_id(value):
         if value <=0:
-            raise ValueError(f"account_id must be positive {value}")
+            raise ValueError(f"salary must be positive, you returned {value}")
         return value
 
 user = Employee(
@@ -30,8 +30,6 @@ user = Employee(
     email='onubawinner@042gmail.com',
     date_of_birth=date(2002,5,11), 
     salary=50000,
-
-    
     department=Departments.IT, 
     elected_benefits=True
     )
@@ -39,13 +37,31 @@ user = Employee(
 # working with dicts
 
 employee_dict = {
-    'name':'Winner', 
+    "name":'Winner', 
     'email':'onubawinner@042gmail.com',
-    'date_of_birth':'2002-05-011', 
-    'salary':50000,
+    'date_of_birth':"2002-05-11", 
+    'salary':"50000",
     'department':'IT', 
     'elected_benefits':True
 }
 
 print(Employee.model_validate(employee_dict))
+print('---------------------------------')
+print(Employee(**employee_dict))
+
+# working with JSONS
+
+employee_json = """
+    {"name":"Winner", 
+    "email":"onubawinner@042gmail.com",
+    "date_of_birth":"2002-05-11", 
+    "salary":50000,
+    "department":"IT", 
+    "employee_id":"74t487y39u94uhiu9-8y84u"
+    "elected_benefits":True}
+    """
+
+print(Employee.model_validate_json(employee_json))
+
+
 
